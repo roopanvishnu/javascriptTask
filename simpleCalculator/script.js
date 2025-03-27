50b0711ace6bfe6da110f44e8b847531ee6dd3db
roopanvishnu <roopanvishnu.work@gmail.com>
2025-03-27 12:33:24 +0530

    simple calculator

---
+++ simpleCalculator/script.js
@@ -0,0 +1,31 @@
+let display = document.getElementById("display");
+let currentInput = "";
+
+function appendToDisplay(value) {
+    if (display.innerText === "0" && value !== ".") {
+        currentInput = value;
+    } else {
+        currentInput += value;
+    }
+    display.innerText = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    display.innerText = "0";
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    display.innerText = currentInput || "0";
+}
+
+function calculateResult() {
+    try {
+        currentInput = eval(currentInput).toString();
+        display.innerText = currentInput;
+    } catch (error) {
+        display.innerText = "Error";
+        currentInput = "";
+    }
+}
